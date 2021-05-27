6cfed41b3d985b91abe6106752aed1b50dcb817a
Maryam Dar <maryamdar@hotmail.com>
2021-05-27 20:54:00 +0100

    task2 completed

--- section1/task2/main.js
+++ section1/task2/main.js
@@ -1,5 +1,10 @@
 // Step 1
 // Take your variables from task 1 and represent them in one object with three keys.
+const details = {
+  name: "Maryam",
+  age: 31,
+  chocolate: true,
+};
 // - A string of your name
 // - A Number of your age
 // - A boolean representing whether you like chocolate
@@ -10,3 +15,11 @@
 //  Print "I LOVE CHOCOLATE!"
 // Else
 //  Print "I'M A LIAR!"
+function step2() {
+  if (details.chocolate === true) {
+    return "I LOVE CHOCOLATE";
+  } else {
+    return "I'M A LIAR!";
+  }
+}
+console.log(step2());
